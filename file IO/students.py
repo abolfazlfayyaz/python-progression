72e70cb56e099edd021af5d37537aaa567478fc5
abolfazlfayyaz <abolfazlfayyaz92@gmail.com>
2024-10-19 09:59:56 +0200

    write on a csv file

--- file IO/students.py
+++ file IO/students.py
@@ -1,10 +1,23 @@
 import csv
-students = []
-with open("file IO/students.csv") as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        students.append({"name": row['name'], "home":row['home']})
+
+# read a csv file
+
+# students = []
+# with open("file IO/students.csv") as file:
+#     reader = csv.DictReader(file)
+#     for row in reader:
+#         students.append({"name": row['name'], "home":row['home']})
 
  
-for student in sorted(students, key=lambda student: student['name']):
-    print(f"{student['name']} is in {student['home']}")
+# for student in sorted(students, key=lambda student: student['name']):
+#     print(f"{student['name']} is in {student['home']}")
+
+
+# write in csv files
+
+name = input("what's your name? ")
+home = input("where is your home? ")
+
+with open ("file IO/students.csv", "a") as file:
+    writer = csv.DictWriter(file, fieldnames=["name", "home"])
+    writer.writerow({"name": name, "home":home})
